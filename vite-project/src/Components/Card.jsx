97fb1e5ae16f951a6e1b896bcb0e628e4e60c6ef
najla <132200719+naj55@users.noreply.github.com>
2023-06-18 11:38:49 +0300

    local

--- vite-project/src/Components/Card.jsx
+++ vite-project/src/Components/Card.jsx
@@ -5,8 +5,8 @@ import { useEffect, useState } from 'react'
 export default function Card() {
     const [title, setTitle] = useState()
     const [body, setbody] = useState()
-    // const title = localStorage.getItem("title")
-    // const body = localStorage.getItem("body")
+    const title1 = localStorage.getItem("title")
+    const body1 = localStorage.getItem("body")
     const id = localStorage.getItem("id")
     useEffect(() => {
         axios.get(`https://64876d1ebeba62972790a0f0.mockapi.io/data/users/${id}`).then(res => {
@@ -17,7 +17,7 @@ export default function Card() {
     }, [])
     return (
         <div>
-            <CardProps title={title} body={body}></CardProps>
+            <CardProps title={title1} body={body1}></CardProps>
 
         </div>
     )
